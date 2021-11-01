3ccfc92bdbf835e83ae281fb01773a86bbc16c4d
johnng0805 <johnng0805@gmail.com>
2021-11-01 11:32:52 +0700

    Backup commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const db = require("./config/db");
+const session = require("express-session");
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,15 @@ const app = express();
 app.set("view engine", "pug");
 app.use("/static", express.static(path.join(__dirname, "public")));
 app.use(express.json());
+
+app.use(session({
+    secret: "scarletty-api",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 30 * 1000 * 60
+    }
+}));
 
 app.listen(process.env.PORT, () => {
     console.log('Starting Scarletty-API at port: ' + process.env.PORT);
